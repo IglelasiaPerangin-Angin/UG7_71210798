e71abb61ea1e6bdfa38b1caa6e9a12403a9f50b4
iglelasia <93341867+iglelasia@users.noreply.github.com>
2022-10-12 19:29:37 +0700

    Add files via upload

--- 71210798_UG7.py
+++ 71210798_UG7.py
@@ -1,70 +1,55 @@
 class StackList:
     def __init__(self):
         self.stack_data = list()
-
     def push(self, new_data):
         self.stack_data.append(new_data)
-
     def top(self):
         if len(self.stack_data) ==0:
             return None
         else:
             return self.stack_data[-1]
-
     def pop(self):
         if len(self.stack_data) == 0:
             return None
         else:
             pop_data = self.stack_data.pop()
             return pop_data
-
     def size(self):
         return len(self.stack_data)
 class UndoRedo:
     def __init__(self):
-        self.mainStack = StackList() #stack ini sebagai tempat menyimpan data pertama kali
-        self.backupStack = StackList() #stack ini sebagai tempat menyimpan data yang di hapus
-
-    def write(self,data):
-        """Pada Suatu Hari hiduplah seorang kakek-kakek"""
-        return self._history
+            self.mainStack = StackList() 
+#stack ini sebagai tempat menyimpan data pertama kali
+            self.backupStack = StackList() 
+#stack ini sebagai tempat menyimpan data yang di hapus
+    def write(self, data):
+        self.mainStack.push(data)
+        print(f"Data {data} berhasil ditambahkan!")
 
     def undo(self):
-         """Dia tinggal sebatang kara di pegunungan"""
-        if self._history_position > 0:
-            self._history_position -= 1
-            self._commands[
-                self._history[self._history_position][1]
-            ].execute(self._history[self._history_position][2])
-        else:
-            print("nothing to undo")
+        self.backupStack.push(self.mainStack.pop())
+        self.printInfo()
 
     def redo(self):
-        """Dia kemudian turun gunng buat kuliah"""
-        if self._history_position + 1 < len(self._history):
-            self._history_position += 1
-            self._commands[
-                self._history[self._history_position][1]
-            ].execute(self._history[self._history_position][2])
-        else:
-            print("nothing to REDO")
+        self.mainStack.push(self.backupStack.pop())
+        self.printInfo()
 
     def printInfo(self):
-        print("nothing to REDO")
+        print(' '.join(self.mainStack.stack_data))
 
-if __name__ == '__main__':
-    obj_undoredo = UndoRedo()
-    obj_undoredo.undo() #Test case Jika belum ada data yang ditambahkan
-    obj_undoredo.redo() #Test case jika belum ada data yang di undo
-    obj_undoredo.write('Pada Suatu Hari hiduplah seorang kakek-kakek')
-    obj_undoredo.write("Dia tinggal sebatang kara di pegunungan")
-    obj_undoredo.write("Dia kemudian turun gunung buat kuliah")
-    obj_undoredo.write("SEMESTER 5 BANYAK TUGASSSSSSS !!!")
-    obj_undoredo.printInfo()
-    obj_undoredo.undo()
-    obj_undoredo.undo()
-    obj_undoredo.undo()
-    obj_undoredo.undo()
-    obj_undoredo.redo()
-    obj_undoredo.redo()
-    obj_undoredo.redo()
+
+obj_undoredo = UndoRedo()
+# obj_undoredo.undo()
+# obj_undoredo.redo()
+obj_undoredo.write('Pada Suatu Hari hiduplah seorang kakek-kakek')
+obj_undoredo.write("Dia tinggal sebatang kara di pegunungan")
+obj_undoredo.write("Dia kemudian turun gunung buat kuliah")
+obj_undoredo.write("SEMESTER 5 BANYAK TUGASSSSSSS !!!")
+obj_undoredo.printInfo()
+obj_undoredo.undo()
+obj_undoredo.undo()
+obj_undoredo.undo()
+obj_undoredo.undo()
+obj_undoredo.redo()
+obj_undoredo.redo()
+obj_undoredo.redo()
